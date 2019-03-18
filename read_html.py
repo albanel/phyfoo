b1836e7eedf10b3c5caf2b52a4491c92826d9213
albanel <albane.lysiak@etudiant.univ-rennes.fr>
2019-03-18 11:32:09 +0100

    18 mars 2019

--- read_html.py
+++ read_html.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 
-liste = []
-
 with open("database_file.txt","r") as db_html :
 	html_doc = db_html.read()
 	soup = BeautifulSoup(html_doc)
-	p = soup.find_all(id="div_homology_member")
-
-#print(liste)
+	p = soup.find_all(id="div_method_link_species_set")
+	for elt in p :
+		list1 = elt.find_all("td")
+	ct = 0
+	print(list1[0])
+	for elt in list1 :
+		if ct % 5 == 0 :
+			elt = str(elt)
+			elt = elt.replace("<b>","</b>")
+			elt = elt.split("</b>")
+			print(elt[1])
+		ct+=1
+	
+	
